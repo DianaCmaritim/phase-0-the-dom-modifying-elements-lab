ba10e0eabbb8fbe90f06fd468088c08630f98c29
Diana <maritimdiane96@gmail.com>
2022-11-20 19:34:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+/*const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "<h1> YOUR-NAME is the champion </h1>"*/
+
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+const myName = "Ali";
+newHeader.textContent = `${myName} is the champion`;
